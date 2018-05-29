68fedaac0dc2ef8ca39994cca1e03a2fc5e6742d
Anchit Shukla <33898271+anchitsh96@users.noreply.github.com>
2018-05-29 11:37:03 +0530

    Update fsr_test.ino

--- fsr_test.ino
+++ fsr_test.ino
@@ -1,24 +1,22 @@
-
-
 int fsr_l = 0;
 int fsr_r = 0;
 
-int fsr_l_pin = A0;
 int fsr_r_pin = A1;
+int fsr_l_pin = A2;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(fsr_l_pin,INPUT);
-  pinMode(fsr_r_pin,INPUT);
+  pinMode(fsr_r_pin, INPUT);
+  pinMode(fsr_l_pin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  fsr_l = analogRead(fsr_l_pin);
   fsr_r = analogRead(fsr_r_pin);
-
-  Serial.println(fsr_l);
+  fsr_l = analogRead(fsr_l_pin);
+  
+  Serial.print(fsr_l);
+  Serial.print(",");
   Serial.println(fsr_r);
   delay(500);
 }
