17fb5ae923f8c98c57a764f8d9d2505122f9c60f
anchitsh96 <anchit96@gmail.com>
2018-06-19 11:00:00 +0000

    changes

--- sensor_fsr/v2.py
+++ sensor_fsr/v2.py
@@ -1,7 +1,7 @@
 """
 .. module:: sensor_fsr/v2
    : platform:
-   : synopsis: 
+   : synopsis:
 
 .. moduleauthor:: @anchitsh96
 
@@ -24,8 +24,8 @@ def average(data):
 SAMPLE = 20
 INTERVAL = 0.005
 GAINVOL = 4.096
-adc = Adafruit_ADS1x15.ADS1115(0x48, busnum=1)
-adc1 = Adafruit_ADS1x15.ADS1115(0x49, busnum=0)
+adc = Adafruit_ADS1x15.ADS1115(0x48, busnum=0)
+adc1 = Adafruit_ADS1x15.ADS1115(0x49, busnum=1)
 CHANNELS_CONNECTED = 2
 fsr = [0]*CHANNELS_CONNECTED
 GAIN=16
@@ -43,9 +43,9 @@ while True:
     f1,f2 = [],[]
     for s in range(SAMPLE):
         #for i in range(CHANNELS_CONNECTED):
-            buf = adc.read_adc(0, gain=GAIN) #- mean
+            buf = adc.read_adc(0, gain=GAIN) - mean
             mv = map(buf,-32768,32767,0,1023)
-            buf1 = adc1.read_adc(0, gain=GAIN)# - mean
+            buf1 = adc1.read_adc(0, gain=GAIN) - mean
             mv1 = map(buf1,-32768,32767,0,1023)
 
             #if i is 0:
