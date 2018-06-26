c0eac93a3c10995e51d652fa9e5ed51d285dd761
remus-lupin <grv.ashu@gmail.com>
2018-06-26 14:01:58 +0530

    updated fsr library

--- reride/fsr.py
+++ reride/fsr.py
@@ -11,25 +11,118 @@ import time
 import Adafruit_ADS1x15
 import Adafruit_GPIO.I2C as I2C
 
-GAINVOL = 4.096
-GAIN=16
-
-adc = []
-adc.append(Adafruit_ADS1x15.ADS1115(0x48, busnum=0))
-adc.append(Adafruit_ADS1x15.ADS1115(0x49, busnum=1))
-
 def map(x, min1, max1, min2, max2):
     steps = (x/(max1-min1))
     buf = steps*(max2-min2) + min2
     return buf
 
-def read_fsr(delay=0.5):
-    fsr = []*8
-    for i in range(4):
-        buf = adc[0].read_adc(i, gain=GAIN)
-        fsr[i] = map(buf, -32768, 32767, 0, 2048)
-    for i in range(4):
-        buf = adc[1].read_adc(i, gain=GAIN)
-        fsr[4+i] = map(buf, -32768, 32767, 0, 32768)
-    time.sleep(delay)
-    return fsr
+class FSR:
+
+    def __init__(self, adc1 = [0x48,0], adc2 = [0x49,1]):
+        """ initialise the ADC and create FSR object
+
+            interfaces the RPi to ADC
+
+            Args:
+                self : address of the FSR object (implicit)
+                adc1 ([hex,int]): Address of ADC1 (Configuration register, Bus Number)
+                adc2 ([hex,int]): Address of ADC2 (Configuration register, Bus Number)
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+        self.adc = []
+        self.adc.append(Adafruit_ADS1x15.ADS1115(adc1[0], busnum=adc1[1]))
+        self.adc.append(Adafruit_ADS1x15.ADS1115(adc2[0], busnum=adc2[1]))
+
+        self.gain = 16
+        self.gain_vol = 4.096
+
+        self.delay = 0.5
+
+        # default adc minimum output of 16Bit
+        self.raw_range = [-32768, 32767]
+        self.mapped_range = [0, 1023]
+
+    def set_gain(self, new_gain):
+        """ change the default gain
+
+            assign new gain to gain attribute
+
+            Args:
+                self : address of the FSR object (implicit)
+                new_gain (int) : new gain value
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+        self.gain = new_gain
+
+    def set_mapped_range(self, new_range):
+        """ change the default output range
+
+            assign new output range to existing range
+
+            Args:
+                self : address of the FSR object (implicit)
+                new_range ([int,int]) : new range
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+        self.mapped_range = new_range
+
+    def set_delay(self, new_delay):
+        """ change the default delay
+
+            assign new delay
+
+            Args:
+                self : address of the FSR object (implicit)
+                new_delay (int) : new delay in microseconds
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+        self.delay = new_delay
+
+    def read_fsr(self, mapped=True, read=[0,1,2,3,4,5,6,7]):
+        fsr = []*8
+        for i in range(4):
+            if i in read:
+                buf = self.adc[0].read_adc(i, gain=self.gain)
+                if mapped is True:
+                    fsr[i] = map(buf, self.raw_range[0], self.raw_range[1], self.mapped_range[0], self.mapped_range[1])
+                else:
+                    fsr[i] = buf
+        for i in range(4):
+            if (4+i) in read:
+                buf = self.adc[1].read_adc(i, gain=self.gain)
+                if mapped is True:
+                    fsr[4+i] = map(buf, self.raw_range[0], self.raw_range[1], self.mapped_range[0], self.mapped_range[1])
+                else:
+                    fsr[4+i] = buf
+        time.sleep(self.delay)
+        return fsr
+
+    def read_fsr_sampled(self, sampling_duration = 0.1, samples = 10, mapped=True, read=[0,1,2,3,4,5,6,7]):
+        fsr_sampled = [0]*8
+
+        for i in samples:
+            fsr = self.read_fsr(self, mapped=mapped, read=read)
+            fsr_sampled[i] = (fsr_sampled[i] + fsr[i])/2
+            time.sleep(sampling_duration)
+
+        return fsr_sampled
--- tests/fsr_test.py
+++ tests/fsr_test.py
@@ -12,7 +12,22 @@ sys.path.append("..")
 
 from reride import fsr
 
-# read 10 values
-for i in range(10):
-    data = fsr.read_fsr(delay=0.3)
-    print(data)
+# # select bus/interface
+#
+# # read 10 values
+# for i in range(10):
+#     data = fsr.read_fsr(delay=0.3)
+#     print(data)
+
+# Default initialisation
+f = fsr.FSR()
+
+# Custom initialisation
+f = fsr.FSR(adc1 = [0x49,1], adc2 = [0x48,0])
+
+f.set_gain(2)
+f.set_mapped_range([0,1023])
+f.set_delay(0.1)
+
+f.read_fsr(read=[0,3], mapped=False)
+f.read_fsr_sampled(sampling_duration=0.2, samples=5)
