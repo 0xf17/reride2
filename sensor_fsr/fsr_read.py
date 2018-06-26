931b331c0fb75a360a66e7d70123ec9503bcf794
anchitsh96 <anchit96@gmail.com>
2018-06-26 09:46:05 +0000

    modified folder and backup in bkp

--- sensor_fsr/fsr_read.py
+++ sensor_fsr/fsr_read.py
@@ -12,14 +12,24 @@ sys.path.append("..")
 
 from reride import fsr
 
+# # select bus/interface
+#
+# # read 10 values
+# for i in range(10):
+#     data = fsr.read_fsr(delay=0.3)
+#     print(data)
+
 # Default initialisation
 f = fsr.FSR()
 
 f.calibrate()
 # Custom initialisation
 #f = fsr.FSR(adc1 = [0x49,1], adc2 = [0x48,0])
+
+#f.set_gain(16)
+#f.set_mapped_range([0,1023])
+#f.set_delay(0.1)
 while True:
     #data=f.read_fsr_sampled(sampling_duration=0.2, samples=5)
     data=f.read_fsr(read=[0,4],mapped=False)
     print(data)
-s
