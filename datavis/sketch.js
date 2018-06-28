4d3d05d631580954fce359bfe88316871a8e812f
anchitsh96 <anchit96@gmail.com>
2018-06-28 06:27:06 +0000

    changes in sketch.js

--- datavis/sketch.js
+++ datavis/sketch.js
@@ -7,45 +7,51 @@ function RL_lean(f1,f2){
 	f_right = f2;
 if (f_right>f_left)
 {
-	var x_lean = map(f_right-f_left,-1023,1023,400,700);
+	var x_lean = map(f_right-f_left,0,1023,400,700);
 }
 else {
-	var x_lean = map(f_left-f_right,-1023,1023,400,700);
+	var x_lean = map(f_right-f_left,-1023,0,100,400);
 }
 return x_lean;
 }
-function FB_lean(f1,f2){
+function FB_lean(f3,f4,f5,f6){
 	//f_right = (f1+f3+f5)/3;
-	f_left = f1;
+	f_forward = (f3+f4)/2;
+	f_backward = (f5+f6)/2;
   //f_left = (f2+f4+f6)/3;
-	f_right = f2;
-if (f_right>f_left)
+if (f_forward>f_backward)
 {
-	var x_lean = map(f_right-f_left,-1023,1023,400,700);
+	var y_lean = map(f_forward-f_backward,0,1023,450,150);
 }
 else {
-	var x_lean = map(f_left-f_right,-1023,1023,400,700);
+	var y_lean = map(f_forward-f_backward,-1023,0,450,750);
 }
-return x_lean;
+return y_lean;
 }
 function setup() {
 	createCanvas(1440,900);
-	x1=RL_lean(745,1012);
-	y1=FB_lean(800,400);
-	frameRate(5);
+	x1=RL_lean(835,820);
+	y1=FB_lean(800,700,700,750);
+	frameRate(1);
 }
 //var a= setInterval(draw,5000);
 function draw() {
-background(255,255,255)
-fill(0,0,0)
-ellipse(x1,y1,40,40);
-x1 = 100+random(600);
-y1 = 100+random(600);
- //await sleep(1000);
-//stroke(0,0,0)
-//line(x1,100,x1,700)
-//stroke(0,0,0)
-//line(100,y1,700,y1)
+	background(250,250,250)
+	stroke(230,230,230)
+	strokeWeight(1)
+	fill(255,255,255)
+	rect(50,100,700,700)
+	stroke(240,240,240)
+	line(x1,150,x1,750)
+	stroke(240,240,240)
+	line(100,y1,700,y1)
+	stroke(255,255,255)
+	strokeWeight(6)
+	fill(80,80,80)
+	ellipse(x1,y1,30,30);
+	x1 = 100+random(600);
+	y1 = 150+random(600);
+
 
 //x1=noise(x1);
 /*if(x1<700 && y1<700){
@@ -58,22 +64,19 @@ y1=y1-5;
 }*/
 stroke(0,0,255)
 strokeWeight(2)
-line(100,100,100,700)
+line(100,150,100,750)
 stroke(0,0,255)
 strokeWeight(2)
-line(700,100,700,700)
+line(700,150,700,750)
 stroke(255,0,0)
 strokeWeight(2)
-line(100,100,700,100)
+line(100,150,700,150)
 stroke(255,0,0)
 strokeWeight(2)
-line(100,700,700,700)
-stroke(255,0,0)
-strokeWeight(2)
-line(100,700,700,700)
-stroke(0,0,0)
-line(100,400,700,400)
-stroke(0,0,0)
-line(400,100,400,700)
+line(100,750,700,750)
+stroke(210,210,210)
+line(100,450,700,450)
+stroke(210,210,210)
+line(400,150,400,750)
 
 }
