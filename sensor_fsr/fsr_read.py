07c7051be8216ca693f7f2a69d8fd42d0a5b0393
anchitsh96 <anchit96@gmail.com>
2018-07-06 09:09:33 +0000

    changes in fsr

--- sensor_fsr/fsr_read.py
+++ sensor_fsr/fsr_read.py
@@ -16,8 +16,8 @@ from reride import fsr
 
 f = fsr.FSR(adc1=[0x48,0])
 f.set_delay(0.8)
-#f.calibrate()
+f.calibrate(read=[0,1,2,3],mapped=True,cancel_noise=True)
 
 while True:
-    data=f.read_fsr(read=[0,1,2,3],mapped=False,cancel_noise=True)
+    data=f.read_fsr(read=[0,1,2,3],mapped=True,cancel_noise=True)
     print(data)
