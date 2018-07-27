dc7d8228e01ac3a273ccbcced920019734625b45
anchitsh96 <anchit96@gmail.com>
2018-07-27 06:23:06 +0000

    added sketch2 gradient box

---
+++ datavis/sketch2_gradientbox.js
@@ -0,0 +1,84 @@
+var fR,fL,fF,fB,a,y,yl,x1,x2,diff,xoff=0,xoff1=0,yoff=0,yoff1=0;
+/*function draw_line(f1,f2,f5,f6){
+  fF=(f1+f2)/2;
+  fB=(f5+f6)/2;
+  diff=fF-fB;
+  if (fF>fB) {
+    while (a<20) {
+    a= round(map(diff,0,1023,0,20));
+    y=300;
+    for (var i=a; i>=0; i--) {
+        x1= 300-sqrt(90000-(y-300)*(y-300));
+        x2= 300+sqrt(90000-(y-300)*(y-300));
+        stroke(mouseY,200,200)
+        strokeWeight(4)
+        line(x1,y,x2,y)
+        y= y-(i*i);
+        //diff=map(noise(xoff),0,1,0,1023);
+        //xoff+=0.22;
+    if (yl<0) {
+      break;
+    }
+   }
+    a=map(noise(xoff),0,1,0,20);
+    xoff+=0.22;
+  }
+ }
+}*/
+function setup(){
+  createCanvas(1440,900)
+  fB=700;
+  fF=1300;
+  fR=900;
+  fL=600;
+  diff=fF-fB;
+  var c1=color(114,168,255);
+  var c2=color(255,255,255);
+  a= round(map(diff,0,1023,0,20));
+  frameRate(100);
+}
+
+function draw(){
+
+  stroke(0,0,200)
+  strokeWeight(10)
+  line(100,450,700,450)
+//draw_line(700,850,400,450)
+  background(255,255,255)
+  stroke(0,0,200)
+  strokeWeight(10)
+  line(200,450,600,450)
+  textSize(32);
+  noStroke()
+  fill(0, 102, 153);
+  text('fB=',750,450);
+  text('fF=',750,500);
+  text(round(fB), 800, 450);
+  text(round(fF),800,500);
+  text(round(diff),900,500);
+
+  y=450;
+  while (diff>0) {
+    for(var i=a;i>=0;i--)
+    b=map(noise(xoff1),0,1,0,60)
+    c=map(noise(yoff1),0,1,0,60)
+    xoff1+=0.0058;
+    yoff1+=0.0054;
+    y=y-(i*i/5);
+
+
+
+
+  }
+  textSize(32);
+  noStroke()
+  fill(0, 102, 153);
+  text('b=',750,100);
+  text('c=',750,150);
+  text(round(b),800,100);
+  text(round(c),800,150);
+  fF=map(noise(xoff),0,1,0,1023);
+  fB=map(noise(xoff*4),0,1,0,1023);
+  a=2+round(map(noise(xoff),0,1,0,18));
+  xoff+=0.0022;
+}
